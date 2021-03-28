2b738350e9cf6183ccabb7e9e2a1815dc4b07d82
Shiva <adiralashiva8@gmail.com>
2021-03-28 18:44:04 +0530

    AxeLibrary doc changes

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # robotframework-axelibrary
 
-Robot Framework keyword library wrapper around axe-selenium-python library
+Robot Framework keyword library wrapper around [axe-selenium-python](https://github.com/mozilla-services/axe-selenium-python) library
+
+## Installation:
+
+ To install robotframework-axelibrary
+ ```
+ $ pip install robotframework-axelibrary
+ ```
+
+## Usage:
+
+ ```
+*** Settings ***
+Library    SeleniumLibrary
+Library    AxeLibrary
+
+*** Test Cases ***
+Google Accessibility Test
+    Open Browser    https://www.google.com/    Chrome
+    &{results}=    Run Accessibility Tests    google.json
+    Log   Violations Count: ${results.violations}
+    Get Json Accessibility Result
+    Log Readable Accessibility Result    violations
+    Log Readable Accessibility Result    incomplete
+    [Teardown]    Close All Browsers
+ ```
+
+## Helpful Link to understand report
+
+ - [Result Arrays](https://github.com/dequelabs/axe-core/blob/master/doc/API.md#result-arrays)
--- src/AxeLibrary/axe.py
+++ src/AxeLibrary/axe.py
@@ -9,21 +9,53 @@ from robot.api import logger
 class AxeLibrary():
 
     ROBOT_LIBRARY_SCOPE = 'GLOBAL'
-  
     ROBOT_LIBRARY_VERSION = VERSION
 
-    @keyword("Perform Accessibility Test")
-    def perform_accessibility_test(self, result_file):
+    def __init__(self):
+        self.axe_instance = None
+        self.results = None
+
+    @keyword("Run Accessibility Tests")
+    def run_accessibility_tests(self, result_file):
+        """
+        Executes accessibility tests in current page by injecting axe-core javascript and write results into `result_file` (json). Return result statisitics
+
+        |  = Attribute =  |  = Description =  |
+        | result_file     |  File to store accessibility test results (.json). Ex: google.json  |
+        """
         # get webdriver instance
         seleniumlib = BuiltIn().get_library_instance('SeleniumLibrary')
         webdriver = seleniumlib.driver
         # create axe instance
-        axe_instance = Axe(webdriver)
+        self.axe_instance = Axe(webdriver)
         # inject axe-core javascript into current page
-        axe_instance.inject()
+        self.axe_instance.inject()
         # run axe accessibility validations
-        results = axe_instance.run()
+        self.results = self.axe_instance.run()
         # write results to specified file
-        axe_instance.write_results(results, result_file)
-        # print results
-        logger.info(axe_instance.report(results["violations"]))
+        self.axe_instance.write_results(self.results, result_file)
+        # generate json
+        result_dict = {"inapplicable":len(self.results["inapplicable"]), "incomplete":len(self.results["incomplete"]),
+         "passes":len(self.results["passes"]), "violations":len(self.results["violations"])} 
+        logger.info(result_dict)
+        # return result
+        return result_dict
+    
+    @keyword("Get Json Accessibility Result")
+    def get_json_accessibility_result(self):
+        """
+        Return accessibility test result in Json format. Need to be used after `Run Accessibility Tests` keyword    
+        """
+        axe_result = json.dumps(self.results, indent = 3)
+        logger.info(axe_result)
+        return axe_result
+    
+    @keyword("Log Readable Accessibility Result")
+    def log_readable_accessibility_result(self, type):
+        """
+        Inserts readable accessibility result into `log.html` based on given `type`. Need to be used after `Run Accessibility Tests` keyword
+
+        |  = Attribute =  |  = Description =  |
+        | Type            |  `violations`, `incomplete` are two supported values  |
+        """
+        logger.info(self.axe_instance.report(self.results[type]))
